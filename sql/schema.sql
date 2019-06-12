3858dcadfb4f25b679640d2cfaebc184512988fc
elizhang227 <zhang.eli@hotmail.com>
2019-06-12 09:47:39 -0400

    created database with schema

---
+++ sql/schema.sql
@@ -0,0 +1,33 @@
+create database budget_app;
+
+create table users
+(
+    id serial primary key,
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(200),
+    password varchar(500)
+);
+
+create table daily
+(
+    id serial primary key,
+    daily_category text,
+    daily_expense float,
+    daily_id integer references users(id)
+);
+
+create table monthly
+(
+    id serial primary key,
+    monthly_category text,
+    monthly_expense float,
+    monthly_id integer references users(id)
+);
+
+create table budget
+(
+    id serial primary key,
+    alloted_budget float,
+    budget_id integer references users(id)
+);
