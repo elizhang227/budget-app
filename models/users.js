8dd5cdb68214e4a4281b4865c37e5169d7b5fc72
elizhang227 <zhang.eli@hotmail.com>
2019-06-13 10:14:25 -0400

    added conditional, when user logs in if they have a budget set already they are directed to the expenses page where they can see their expenses, if not they are directed to the setup page where they can setup their budget

--- models/users.js
+++ models/users.js
@@ -52,6 +52,32 @@ class User {
         return err.message
     }
   }
+
+  static async getUser(email) {
+    try {
+        const response = await db.one(`
+        select id 
+        from users 
+        where email='${email}'`);
+        return response;
+    } catch(err) {
+        return err.message
+    }
+}
+
+  static async budgetExists(id) {
+    try {
+      const response = await db.one(`
+      SELECT alloted_budget
+      FROM budget
+      WHERE budget_id=${id}`);
+      console.log("this is the response of budget", response);
+      return response;
+    } catch(err) {
+      return err.message;
+    }
+  }
+
 }
 
 module.exports = User;
