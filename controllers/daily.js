7909451e708c5b568f20390c6bc268eee14dcce0
elizhang227 <zhang.eli@hotmail.com>
2019-06-16 15:38:41 -0400

    added controller for /daily/expenses

--- controllers/daily.js
+++ controllers/daily.js
@@ -1,4 +1,5 @@
-const monthlyModel = require('../models/daily');
+const monthlyModel = require('../models/daily'),
+    moment = require('moment');
 
 exports.homepage_get = async (req, res) => {
     if(!!req.session.is_logged_in) {
@@ -14,6 +15,47 @@ exports.homepage_get = async (req, res) => {
             },
             partials: {
                 content: 'partial-daily-home'
+            }
+        });
+    } else {
+        res.redirect('/users/login');
+    }
+}
+
+exports.expenses_get = async (req, res) => {
+    if(!!req.session.is_logged_in) {
+        const id = await monthlyModel.getUser(req.session.email);
+        let dailyExpense = await monthlyModel.getTotalDailyExpense(id.id);
+        if (dailyExpense.total === null) {
+            dailyExpense.total = 0;
+        }
+        const refreshTime = await monthlyModel.getTimestamp(id.id);
+        const currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+        let remainingBalance;
+        if ('June 20th 2019, 2:12:11 am' === refreshTime.reset_time) { //'June 20th 2019, 2:12:11 am'
+            await monthlyModel.resetBudget(refreshTime.set_budget, id.id)
+            .then(async() => {
+                remainingBalance = await monthlyModel.getRemainingBalance(id.id);
+                await monthlyModel.clearExpense();
+            })
+        } else {
+            remainingBalance = await monthlyModel.getRemainingBalance(id.id);
+        }
+        console.log("this is remainingBalance", remainingBalance);
+        const listOfExpenses = await monthlyModel.getListOfExpenses(id.id);
+        res.render('template', {
+            locals: {
+                title: `Welcome to my dungeon`,
+                listOfExpenses: listOfExpenses,
+                expenseList: dailyExpense,
+                balance: remainingBalance,
+                is_logged_in: req.session.is_logged_in,
+                userName: req.session.first_name,
+                email: req.session.email
+            },
+            partials: {
+                content: 'partial-daily'
             }
         });
     } else {
--- routes/daily.js
+++ routes/daily.js
@@ -6,46 +6,7 @@ const express = require('express'),
 
 router.get('/', dailyController.homepage_get);
 
-router.get('/expenses', async (req, res, next) => {
-    if(!!req.session.is_logged_in) {
-        const id = await monthlyModel.getUser(req.session.email);
-        let dailyExpense = await monthlyModel.getTotalDailyExpense(id.id);
-        if (dailyExpense.total === null) {
-            dailyExpense.total = 0;
-        }
-        const refreshTime = await monthlyModel.getTimestamp(id.id);
-        const currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-
-        let remainingBalance;
-        if ('June 20th 2019, 2:12:11 am' === refreshTime.reset_time) { //'June 20th 2019, 2:12:11 am'
-            await monthlyModel.resetBudget(refreshTime.set_budget, id.id)
-            .then(async() => {
-                remainingBalance = await monthlyModel.getRemainingBalance(id.id);
-                await monthlyModel.clearExpense();
-            })
-        } else {
-            remainingBalance = await monthlyModel.getRemainingBalance(id.id);
-        }
-        console.log("this is remainingBalance", remainingBalance);
-        const listOfExpenses = await monthlyModel.getListOfExpenses(id.id);
-        res.render('template', {
-            locals: {
-                title: `Welcome to my dungeon`,
-                listOfExpenses: listOfExpenses,
-                expenseList: dailyExpense,
-                balance: remainingBalance,
-                is_logged_in: req.session.is_logged_in,
-                userName: req.session.first_name,
-                email: req.session.email
-            },
-            partials: {
-                content: 'partial-daily'
-            }
-        });
-    } else {
-        res.redirect('/users/login');
-    }
-})
+router.get('/expenses', dailyController.expenses_get);
 
 router.get('/history', async (req, res, next) => {
     if(!!req.session.is_logged_in) {
