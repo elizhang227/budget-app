cedc2cd604e4f8565447c7b18c712eb8b1115e00
elizhang227 <zhang.eli@hotmail.com>
2019-06-16 16:20:09 -0400

    changed conditionals for setup post, was incorrect and didn't check correct for correct conditions

--- controllers/setup.js
+++ controllers/setup.js
@@ -19,3 +19,38 @@ exports.homepage_get = async (req, res) => {
         res.redirect('/users/login');
     }
 }
+
+exports.setup_post = async (req, res) => {
+    if(!!req.session.is_logged_in) {
+        //if budget Exists then run updateBudget, else run setBudget
+        const userID = await setupModel.getUser(req.session.email);
+        const check = await setupModel.budgetExists(userID.id);
+        const date = moment().format('LLL');
+        const refresh = moment().add(7, 'days').format('LLL');
+        console.log(refresh);
+        if(check.alloted_budget === null) {
+            console.log("why is this null?");
+            const { budget } = req.body;
+            setupModel.updateBudget(budget, userID.id)
+            .then(() => {
+                res.redirect('../daily/expenses');
+            });
+            setupModel.budgetTimestamp(budget, date, refresh, userID.id);
+        } else if (check.alloted_budget != null) {
+            console.log("this is an object");
+            const { budget } = req.body;
+            setupModel.updateBudget(budget, userID.id)
+            .then(() => {
+                res.redirect('../daily/expenses');
+            });
+        } else if (typeof check.alloted_budget != 'object') {
+            console.log("this is not an object");
+            const { budget } = req.body;
+            setupModel.setBudget(budget, userID.id)
+            .then(() => {
+                res.redirect('../daily/expenses');
+            })
+            setupModel.budgetTimestamp(budget, date, refresh, userID.id);
+        }
+    }
+}
--- routes/setup.js
+++ routes/setup.js
@@ -1,41 +1,8 @@
 const express = require('express'),
     router = express.Router(),
-    setupController = require('../controllers/setup'),
-    setupModel = require('../models/setup'),
-    moment = require('moment');
-
+    setupController = require('../controllers/setup');
 
 router.get('/', setupController.homepage_get);
-
-router.post('/', async function(req, res, next) {
-    //if budget Exists then run updateBudget, else run setBudget
-    const userID = await setupModel.getUser(req.session.email);
-    const check = await setupModel.budgetExists(userID.id);
-    const date = moment().format('LLL');
-    const refresh = moment().add(7, 'days').format('LLL');
-    console.log(refresh);
-    if(check.alloted_budget === null) {
-        const { budget } = req.body;
-        setupModel.updateBudget(budget, userID.id)
-        .then(() => {
-            res.redirect('../daily/expenses');
-        });
-        setupModel.budgetTimestamp(budget, date, refresh, userID.id);
-    } else if (typeof check.alloted_budget != 'object') {
-        const { budget } = req.body;
-        setupModel.setBudget(budget, userID.id)
-        .then(() => {
-            res.redirect('../daily/expenses');
-        })
-        setupModel.budgetTimestamp(budget, date, refresh, userID.id);
-    } else {
-        const { budget } = req.body;
-        setupModel.updateBudget(budget, userID.id)
-        .then(() => {
-            res.redirect('../daily/expenses');
-        });
-    }
-
-});
+router.post('/', setupController.setup_post);
 
 module.exports = router;
