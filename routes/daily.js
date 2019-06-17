b73f76d96295fd874a6e80326873c5576edd2761
elizhang227 <zhang.eli@hotmail.com>
2019-06-17 19:03:25 -0400

    changed some colors

--- routes/daily.js
+++ routes/daily.js
@@ -2,7 +2,6 @@ const express = require('express'),
     router = express.Router(),
     dailyController = require('../controllers/daily');
 
-router.get('/', dailyController.homepage_get);
 router.get('/expenses', dailyController.weekly_expenses_get);
 router.get('/history', dailyController.history_get);
 router.get('/expenses/:category', dailyController.expenses_by_category_get);
