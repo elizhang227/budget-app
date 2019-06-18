9f48328c0fa076ecf07b88a1307afcee1e091072
elizhang227 <zhang.eli@hotmail.com>
2019-06-18 08:39:24 -0400

    removed commented code

--- controllers/daily.js
+++ controllers/daily.js
@@ -1,27 +1,6 @@
 const monthlyModel = require('../models/daily'),
     moment = require('moment');
 
-// exports.homepage_get = async (req, res) => {
-//     if(!!req.session.is_logged_in) {
-//         const id = await monthlyModel.getUser(req.session.email);
-//         const balance = await monthlyModel.getRemainingBalance(id.id);
-//         res.render('template', {
-//             locals: {
-//                 title: `Welcome to my dungeon`,
-//                 balance: balance,
-//                 is_logged_in: req.session.is_logged_in,
-//                 userName: req.session.first_name,
-//                 email: req.session.email
-//             },
-//             partials: {
-//                 content: 'partial-daily-home'
-//             }
-//         });
-//     } else {
-//         res.redirect('/users/login');
-//     }
-// }
-
 exports.weekly_expenses_get = async (req, res) => {
     if(!!req.session.is_logged_in) {
         const id = await monthlyModel.getUser(req.session.email);
