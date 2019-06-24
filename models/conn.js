136d7c4f3cdb4f5ee7867553c8038115e41b97f7
Ubuntu <ubuntu@ip-172-31-42-99.us-east-2.compute.internal>
2019-06-24 18:23:21 +0000

    added password to conn js file

--- models/conn.js
+++ models/conn.js
@@ -7,7 +7,8 @@ const pgp = require('pg-promise') ({
 const options = {
     host: 'localhost',
     database: 'budget_app',
-    user: 'jctak'
+    user: 'eli',
+    password: 'password'
 }
 
 const db = pgp(options);
